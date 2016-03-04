d7c581eff6059d8cc1842ce1379a8f66387199f3
Jeremiah Wala <jwala@broadinstitute.org>
2016-03-03 22:15:56 -0500

    travis attempt fix3

--- install_bioc.sh
+++ install_bioc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Rscript -e source(https://bioconductor.org/biocLite.R); biocLite(BiocInstaller)"
-Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller")'
+Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); library(BSgenome.Hsapiens.UCSC.hg19); library(GenomicRanges);'
