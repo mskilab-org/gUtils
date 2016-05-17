dcdbd77a3ed4ba394eab81042da234f621b577ad
Marcin Imielinski <mimielinski@nygenome.org>
2016-05-16 21:27:13 -0400

    msg:

--- install_bioc.sh
+++ install_bioc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges")'
+Rscript -e 'install.packages("devtools"); library(devtools);  source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges")'
