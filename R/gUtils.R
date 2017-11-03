dc7d2f39fcb55f2a6457a43eca32c2e998bd5795
xtYao <xiaotong.yao@nyu.edu>
2017-11-03 16:31:46 -0400

    ra.duplicated function

--- R/gUtils.R
+++ R/gUtils.R
@@ -3315,6 +3315,44 @@ ra.dedup = function(grl, pad=500, ignore.strand=FALSE){
     }
 }
 
+#' Show if junctions are Deduplicated
+#'
+#' Determines overlaps between two or more piles of rearrangement junctions (as named or numbered arguments) +/- padding
+#' and will merge those that overlap into single junctions in the output, and then keep track for each output junction which
+#' of the input junctions it was "seen in" using logical flag  meta data fields prefixed by "seen.by." and then the argument name
+#' (or "seen.by.ra" and the argument number)
+#'
+#' @author Xiaotong Yao
+#' @param grl GRangesList representing rearrangements to be merged
+#' @param pad non-negative integer specifying padding
+#' @param ignore.strand whether to ignore strand (implies all strand information will be ignored, use at your own risk)
+#' @return \code{GRangesList} of merged junctions with meta data fields specifying which of the inputs each outputted junction was "seen.by"
+#' @name ra.duplicated
+#' @examples
+#'
+#' @export
+ra.duplicated = function(grl, pad=500, ignore.strand=FALSE){
+    ## TODO: deduplicate it self
+    if (!is(grl, "GRangesList")) stop("Input must be GRangesList!")
+
+    if (any(elementNROWS(grl)!=2)) stop("Each element must be length 2!")
+
+    if (length(grl)==0) return(logical(0))
+
+    if (length(grl)==1) return(FALSE)
+
+    if (length(grl)>1){
+        ix.pair = as.data.table(
+            ra.overlaps(grl, grl, pad=pad, ignore.strand = ignore.strand))[ra1.ix!=ra2.ix]
+        if (nrow(ix.pair)==0){
+            return(rep(FALSE, length(grl)))
+        } else {
+            dup.ix = unique(rowMax(as.matrix(ix.pair)))
+            return(seq_along(grl) %in% dup.ix)
+        }
+    }
+}
+
 ## #' Calc pairwise distance for rearrangements represented by \code{GRangesList} objects
 ## #'
 ## #' return length(grl) by length(grl) numeric matrix of pairwise distances between
