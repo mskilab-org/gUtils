25e4a453a77aeaa25272d6d413cc3156b0b6b2e3
micoli98 <102139056+micoli98@users.noreply.github.com>
2024-02-12 15:50:33 +0200

    Update gUtils.R
    Removed the "formula =" from the function stats::aggregate inside grl.in

--- R/gUtils.R
+++ R/gUtils.R
@@ -2107,7 +2107,7 @@ grl.in = function(grl, windows, some = FALSE, only = FALSE, logical = TRUE, exac
                             split(m$query.id, factor(m$grl.id, 1:length(grl)))))
     }
     else{
-        tmp = stats::aggregate(formula = subject.id ~ grl.id, data = m, FUN = function(x) numwin-length(setdiff(1:numwin, x)))
+        tmp = stats::aggregate(subject.id ~ grl.id, data = m, FUN = function(x) numwin-length(setdiff(1:numwin, x)))
     }
 
     out = rep(FALSE, length(grl))
